1517fe4277e99ce09576d1e022d506b642f5d087
Siva Prasad <sivaprasad.ummaneni@gmail.com>
2025-02-20 14:36:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const buttons = document.querySelectorAll('.add-to-cart');
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            alert('Product added to cart!');
+        });
+    });
+});
